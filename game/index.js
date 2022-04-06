ddeefeec80ae9411fe6bc29481d3e13f9b0c367a
NailsonChagas <chagasnailson@gmail.com>
2022-04-05 22:16:28 -0300

    terminado

--- game/index.js
+++ game/index.js
@@ -1,6 +1,10 @@
 import Player from "./player.js";
 import Projectile from "./projectile.js";
 import Enemy  from "./enemy.js";
+import Particle from "./particle.js";
+
+const START = document.getElementById("start");
+const MODAL = document.getElementById("modelEl");
 
 //selecionar canvas
 const CANVAS = document.getElementById('canvas_element');
@@ -15,15 +19,31 @@ CANVAS.height = window.innerHeight;
 //iniciando player
 const X = CANVAS.width/2;
 const Y = CANVAS.height/2;
-const PLAYER = new Player(X, Y, 30, 'blue');
 
-//projeteis e inimigos
-const PROJECTILES = [];
-const ENEMIES = [];
+
+let score = 0; //adicionar sistema de score (falta escrever no canvas)
+const SCORE = document.getElementById("score");
+const BScore = document.getElementById("BScore");
+
+//player, projeteis, particulas e inimigos
+let PLAYER = new Player(X, Y, 30, 'white');
+let PROJECTILES = [];
+let PARTICLES = [];
+let ENEMIES = [];
+
+function init(){
+    PLAYER = new Player(X, Y, 30, 'white');
+    PROJECTILES = [];
+    PARTICLES = [];
+    ENEMIES = [];
+    score = 0;
+    SCORE.innerText = score;
+    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+}
 
 function spawnEnemies(){
     setInterval(() => {
-        let radius = Math.random() * (30 - 4) + 4; // de 4px a 30px
+        let radius = Math.random() * (50 - 7) + 7; // de 7px a 50px
         let x;
         let y;
 
@@ -36,7 +56,7 @@ function spawnEnemies(){
             y = Math.random() < 0.5 ? 0 - radius : CANVAS.height + 30;
         }
         
-        let color = 'green'
+        let color = `hsl(${Math.random() * 360}, 50%, 50%)`
 
         let angle = Math.atan2(
             Y - y,
@@ -57,12 +77,50 @@ function spawnEnemies(){
 let animationID;
 function animate(){ //loop do jogo
     animationID = requestAnimationFrame(animate);
-    CTX.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    console.log(score)
+
+    CTX.fillStyle = 'rgba(0,0,0, 0.2)'
+    CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
     
     PLAYER.draw(CTX);
+
+    //desenhar particulas
+    PARTICLES.forEach((particle, idx) => {
+        if(particle.alpha <=0){
+            PARTICLES.splice(idx, 1)
+        }
+        else{
+            particle.update(CTX);
+        }
+    });
     
-    PROJECTILES.forEach((projectile) => {
+    PROJECTILES.forEach((projectile, idxP) => {
+        let dist = Math.hypot(projectile.x - PLAYER.x, projectile.y - PLAYER.y);
+
         projectile.update(CTX);
+        
+        if(
+            projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > CANVAS.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > CANVAS.height
+        ){ //apagando projeiteis fora da tela
+            setTimeout(() => { //usando para impedir ser apagado antes de ser desenhado
+                PROJECTILES.splice(idxP, 1);
+            }, 0)
+        }
+
+        if((dist - projectile.radius - PLAYER.radius < 1) && projectile.hits !=2){ //colisão player projetil
+            if(projectile.hits < 2){
+                score += 10;
+                SCORE.innerText = score; //mudando valor do score no html
+            }
+            setTimeout(() => { //usando para impedir ser apagado antes de ser desenhado
+                PROJECTILES.splice(idxP, 1);
+            }, 0)
+        }
+
+
     });
     
     ENEMIES.forEach((enemy, idxE) => {
@@ -72,8 +130,9 @@ function animate(){ //loop do jogo
 
         //colisão com player = fim
         if(distP - enemy.radius - PLAYER.radius < 1){
-            console.log("colisão");
             cancelAnimationFrame(animationID);
+            MODAL.style.display = 'flex';
+            BScore.innerText = score;
         }
 
         PROJECTILES.forEach((projectile, idxP) => {
@@ -81,10 +140,47 @@ function animate(){ //loop do jogo
             
             //colisão com projetil
             if(dist - enemy.radius - projectile.radius < 1){
-                
+                projectile.hits -= 1;
+                projectile.radius /= 2;
+
+                //particulas
+                for(let i = 0; i < enemy.radius * 2; i++){
+                    PARTICLES.push(new Particle(
+                            projectile.x, 
+                            projectile.y,
+                            Math.random() * 3,
+                            enemy.color,
+                            {
+                                x: (Math.random() - 0.5) * (Math.random() * 5),
+                                y: (Math.random() - 0.5) * (Math.random() * 5),
+                            }
+                        )
+                    );
+                }
+
+
                 setTimeout(() => { //usando para impedir ser apagado antes de ser desenhado
-                    ENEMIES.splice(idxE, 1);
-                    PROJECTILES.splice(idxP, 1);
+                    if(enemy.radius - 10 > 5){
+                        //usando gsap para fazer uma interpolação
+                        gsap.to(enemy, {
+                            radius: enemy.radius - 10
+                        });
+                        
+                    }
+                    else{
+                        ENEMIES.splice(idxE, 1);
+                    }
+                    
+
+                    if(projectile.hits == 0){
+                        PROJECTILES.splice(idxP, 1);
+                    }
+                    else{
+                        projectile.color = enemy.color;
+                        projectile.velocity.x = projectile.velocity.x * -1.5;
+                        projectile.velocity.y = projectile.velocity.y * -1.5;
+                    }
+                    
                 }, 0)
                 
             }
@@ -94,7 +190,6 @@ function animate(){ //loop do jogo
 
 }
 
-
 //inserindo leitor de evento na página
 window.addEventListener("click", (event) => {
     let angle = Math.atan2(
@@ -103,20 +198,26 @@ window.addEventListener("click", (event) => {
     );
 
     let velocity = {
-        x: Math.cos(angle) * 3,
-        y: Math.sin(angle) * 3
+        x: Math.cos(angle) * 6,
+        y: Math.sin(angle) * 6
     }
    
     //a cada clique gerar um projetil
     PROJECTILES.push(
         new Projectile(
             X, Y, 5, 
-            'red', 
+            'white', 
             velocity
         )
     );
     
 });
 
-animate();
-spawnEnemies();
+
+START.addEventListener("click", (event) => {
+    MODAL.style.display = 'none';
+    init();
+    animate();
+    spawnEnemies();
+});
+
--- game/particle.js
+++ game/particle.js
@@ -1,25 +1,33 @@
-class Projectile {
+const friction = 0.985;
+
+class Particle {
     constructor(x, y, radius, color, velocity){
         this.x = x;
         this.y = y;
         this.radius = radius;
         this.color = color;
         this.velocity = velocity;
-        this.hits = 2
+        this.alpha = 1;
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.globalAlpha = this.alpha;
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);//forma a ser desenhada
         ctx.fillStyle = this.color;
         ctx.fill(); //desenhar
+        ctx.restore();
     }
 
     update(ctx) {
         this.draw(ctx)
+        this.velocity.x *= friction;
+        this.velocity.y *= friction;
         this.x = this.x + this.velocity.x;
         this.y = this.y + this.velocity.y;
+        this.alpha -= 0.01;
     }
 }
 
-export default Projectile;
+export default Particle;
